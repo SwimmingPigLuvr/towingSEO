0330198edefbc288b8a397200ac7c77fcb7c337c
admin <admin@admins-Laptop.lan>
2024-03-25 12:55:35 -0700

    added a user testimonials section

--- script.js
+++ script.js
@@ -21,3 +21,23 @@ document.addEventListener('click', function(e) {
         dropdownContent.style.display = 'none';
     }
 });
+
+
+// carousel
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function moveSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("carousel-item");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
